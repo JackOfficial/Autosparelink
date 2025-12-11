2204a5bdb3433d424908e20ab109c233c8df911b
JackOfficial <musengimanajacques@gmail.com>
2025-12-11 04:55:29 +0200

    Updated Logo on mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,8 +111,7 @@
             <div class="col-lg-9">
                 <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
                     <a href="{{ route('home') }}" class="text-decoration-none d-block d-lg-none">
-                        <span class="h1 text-uppercase text-dark bg-light px-2">AutoSpare</span>
-                        <span class="h1 text-uppercase text-light bg-primary px-2 ml-n1">Parts</span>
+                    <img src="{{ asset('frontend/img/logo.png') }}" class="w-25">
                     </a>
                     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                         <span class="navbar-toggler-icon"></span>
