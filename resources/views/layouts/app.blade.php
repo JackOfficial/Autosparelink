fcd620818956b92c5e0415746268a6eff5b2c045
JackOfficial <musengimanajacques@gmail.com>
2025-12-12 17:14:33 +0200

    Placed resources links to the layout page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -124,6 +124,7 @@
             <!-- Navbar Items -->
             <div class="collapse navbar-collapse" id="mainNavbar">
                 <ul class="navbar-nav mr-auto ml-4">
+                    <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="/about">About Us</a></li>
                     <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
                     <li class="nav-item"><a class="nav-link" href="/shop">Shop</a></li>
