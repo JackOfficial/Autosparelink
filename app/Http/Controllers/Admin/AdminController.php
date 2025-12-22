76b760253ee562f0085b4968d383567dc75d0a23
JackOfficial <musengimanajacques@gmail.com>
2025-12-22 07:12:06 +0200

    Resolving Issues

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -14,21 +14,21 @@ use App\Models\Order;
 class AdminController extends Controller
 {
     public function index(){
-        // $brands = Brand::count();
-        // $vehicle_models = VehicleModel::count();
-        // $parts = Part::count();
-        // $users = User::count();
-        // $recentOrders = Order::where('status', 'Pending')->count();
-        // $salesMonths = ["Jan", "Feb", "Mar", "Apr", "May"];
-        // $salesData = [120, 190, 300, 500, 200];
-        // $inventoryData = [300, 50, 20]; // In Stock, Low Stock, Out of Stock
+        $brands = Brand::count();
+        $vehicle_models = VehicleModel::count();
+        $parts = Part::count();
+        $users = User::count();
+        $recentOrders = Order::where('status', 'Pending')->count();
+        $salesMonths = ["Jan", "Feb", "Mar", "Apr", "May"];
+        $salesData = [120, 190, 300, 500, 200];
+        $inventoryData = [300, 50, 20]; // In Stock, Low Stock, Out of Stock
 
-        // $pendingOrders = Order::where('status', 'Pending')->count();
-        // $lowStockParts = Part::where('stock_quantity', '<', 5)->count();
-        // $recentOrders = Order::latest()->take(5)->get();
+        $pendingOrders = Order::where('status', 'Pending')->count();
+        $lowStockParts = Part::where('stock_quantity', '<', 5)->count();
+        $recentOrders = Order::latest()->take(5)->get();
 
 
-        // return view('admin.index', compact('brands', 'parts', 'vehicle_models', 'pendingOrders', 'lowStockParts', 'recentOrders', 'users', 'recentOrders', 'salesMonths', 'salesData', 'inventoryData'));
+        return view('admin.index', compact('brands', 'parts', 'vehicle_models', 'pendingOrders', 'lowStockParts', 'recentOrders', 'users', 'recentOrders', 'salesMonths', 'salesData', 'inventoryData'));
     }
 
     public function addTask(Request $request){
