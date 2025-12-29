d5c0a858748c1f472798443f7f14a9f733345f30
JackOfficial <musengimanajacques@gmail.com>
2025-12-29 04:55:07 +0200

    Part Brands Pages

--- app/Http/Controllers/BrandsController.php
+++ app/Http/Controllers/BrandsController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
-use App\Models\PartBrands;
+use App\Models\PartBrand;
 use Illuminate\Http\Request;
 
 class BrandsController extends Controller
 {
    public function brands(){
      $vehicle_brands = Brand::orderBy('brand_name')->get();
-    $parts_brands   = PartBrands::orderBy('name')->get();
+    $parts_brands   = PartBrand::orderBy('name')->get();
 
     return view('brands', compact('vehicle_brands', 'parts_brands'));
    }
