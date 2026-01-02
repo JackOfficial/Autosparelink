f36b2c8c3597c91ca5880e22f13d2941daccb995
JackOfficial <musengimanajacques@gmail.com>
2026-01-02 13:38:15 +0200

    Part Brands Pages

--- app/Models/Part.php
+++ app/Models/Part.php
@@ -40,6 +40,16 @@ class Part extends Model
    return $this->hasMany(PartFitment::class);
 }
 
+public function specifications()
+{
+    return $this->belongsToMany(
+        Specification::class,
+        'part_fitments',
+        'part_id',
+        'specification_id'
+    )->withTimestamps();
+}
+
  public function photos()
 {
    return $this->hasMany(PartPhoto::class);
