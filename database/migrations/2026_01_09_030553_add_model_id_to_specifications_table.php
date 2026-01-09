da20ba071091366b86529ebbca1e4fd08c7eb3b7
JackOfficial <musengimanajacques@gmail.com>
2026-01-09 05:37:16 +0200

    refinement

--- database/migrations/2026_01_09_030553_add_model_id_to_specifications_table.php
+++ database/migrations/2026_01_09_030553_add_model_id_to_specifications_table.php
@@ -1,5 +1,4 @@
 <?
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -9,29 +8,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('specifications', function (Blueprint $table) {
-                $table->dropForeign(['variant_id']);  // Drop foreign key first
 
-    $table->unsignedBigInteger('variant_id')->nullable()->change(); // Modify column to nullable
-
-    $table->foreign('variant_id')->references('id')->on('variants')->cascadeOnDelete(); // Recreate foreign key
-
-    if (!Schema::hasColumn('specifications', 'vehicle_model_id')) {
-        $table->foreignId('vehicle_model_id')->nullable()->after('variant_id')->constrained()->cascadeOnDelete();
-    }
+            if (!Schema::hasColumn('specifications', 'vehicle_model_id')) {
+                $table->foreignId('vehicle_model_id')->nullable()->after('variant_id')->constrained()->cascadeOnDelete();
+            }
         });
     }
 
     public function down(): void
     {
         Schema::table('specifications', function (Blueprint $table) {
-            $table->dropForeign(['variant_id']);
             if (Schema::hasColumn('specifications', 'vehicle_model_id')) {
                 $table->dropForeign(['vehicle_model_id']);
-            }
-            $table->foreignId('variant_id')->nullable(false)->change();
-            $table->foreign('variant_id')->references('id')->on('variants')->cascadeOnDelete();
-            if (Schema::hasColumn('specifications', 'vehicle_model_id')) {
-                $table->dropColumn('vehicle_model_id');
             }
         });
     }
