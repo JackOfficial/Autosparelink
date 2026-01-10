af154fba17b7b8122c521dfe54c58610d0360833
JackOfficial <musengimanajacques@gmail.com>
2026-01-10 16:27:32 +0200

    Prepare for Livewire reinstall and push

--- resources/views/livewire/admin/variant-form.blade.php
+++ resources/views/livewire/admin/variant-form.blade.php
@@ -6,6 +6,7 @@
     <form wire:submit.prevent="save">
         <div class="card-body">
 
+            {{-- SUCCESS --}}
             @if (session()->has('success'))
                 <div class="alert alert-success">{{ session('success') }}</div>
             @endif
@@ -16,27 +17,27 @@
 
                 <div class="row">
                     <div class="col-md-4">
-                        <label>Brand <span class="text-danger">*</span></label>
+                        <label>Brand *</label>
                         <select wire:model.live="brand_id" class="form-control">
                             <option value="">Select Brand</option>
                             @foreach($brands as $brand)
                                 <option value="{{ $brand->id }}">{{ $brand->brand_name }}</option>
                             @endforeach
                         </select>
-                        @error('brand_id') <span class="text-danger">{{ $message }}</span> @enderror
+                        @error('brand_id') <small class="text-danger">{{ $message }}</small> @enderror
                     </div>
 
                     <div class="col-md-4">
-                        <label>Vehicle Model <span class="text-danger">*</span></label>
+                        <label>Vehicle Model *</label>
                         <select wire:model.live="vehicle_model_id"
                                 class="form-control"
-                                @if(empty($vehicleModels)) disabled @endif>
+                                @disabled(empty($vehicleModels))>
                             <option value="">Select Model</option>
                             @foreach($vehicleModels as $model)
                                 <option value="{{ $model->id }}">{{ $model->model_name }}</option>
                             @endforeach
                         </select>
-                        @error('vehicle_model_id') <span class="text-danger">{{ $message }}</span> @enderror
+                        @error('vehicle_model_id') <small class="text-danger">{{ $message }}</small> @enderror
                     </div>
                 </div>
             </fieldset>
@@ -47,75 +48,60 @@
 
                 <div class="row">
                     <div class="col-md-6">
-                        <label>Variant Name <span class="text-danger">*</span></label>
-                        <input type="text"
-                               wire:model="name"
-                               class="form-control"
-                               placeholder="e.g. Corolla 1.8L Sedan">
-                        @error('name') <span class="text-danger">{{ $message }}</span> @enderror
+                        <label>Variant Name *</label>
+                        <input type="text" wire:model="name" class="form-control">
+                        @error('name') <small class="text-danger">{{ $message }}</small> @enderror
                     </div>
 
                     <div class="col-md-6">
                         <label>Trim Level</label>
-                        <input type="text"
-                               wire:model="trim_level"
-                               class="form-control"
-                               placeholder="LE, SE, Sport">
-                    </div>
-                </div>
-            </fieldset>
-
-            {{-- IDENTIFIERS --}}
-            <fieldset class="border p-3 mb-4">
-                <legend class="w-auto px-2">Identifiers</legend>
-
-                <div class="row">
-                    <div class="col-md-4">
-                        <label>Chassis Code</label>
-                        <input type="text" wire:model="chassis_code" class="form-control">
-                    </div>
-
-                    <div class="col-md-4">
-                        <label>Model Code</label>
-                        <input type="text" wire:model="model_code" class="form-control">
-                    </div>
-
-                    <div class="col-md-4">
-                        <label>Status</label>
-                        <select wire:model="status" class="form-control">
-                            <option value="1">Active</option>
-                            <option value="0">Inactive</option>
-                        </select>
+                        <input type="text" wire:model="trim_level" class="form-control">
                     </div>
                 </div>
             </fieldset>
 
             {{-- MEDIA --}}
             <fieldset class="border p-3 mb-4">
-                <legend class="w-auto px-2">Media</legend>
+                <legend class="w-auto px-2">Photos</legend>
 
-                <div class="row">
-                    <div class="col-md-6">
-                        <label>Default Photo</label>
-                        <input type="file" wire:model="photo" class="form-control" accept="image/*">
+                <input type="file" wire:model="photos" multiple class="form-control">
 
-                        @if ($photo)
-                            <div class="mt-2">
-                                <img src="{{ $photo->temporaryUrl() }}"
-                                     class="img-thumbnail"
-                                     style="max-width:160px;">
-                            </div>
-                        @endif
-
-                        <small class="text-muted">Optional default image.</small>
-                    </div>
+                <div class="mt-2 d-flex flex-wrap gap-2">
+                    @foreach($photos as $photo)
+                        <img src="{{ $photo->temporaryUrl() }}"
+                             class="img-thumbnail"
+                             style="max-width:120px;">
+                    @endforeach
                 </div>
+            </fieldset>
+
+            {{-- SPECIFICATIONS QUESTION --}}
+            <fieldset class="border p-3 mb-4">
+                <legend class="w-auto px-2">Specifications</legend>
+
+                <label>Does this variant have specifications?</label>
+
+                <div class="mt-2">
+                    <label class="mr-3">
+                        <input type="radio" wire:model="has_specifications" value="1">
+                        Yes
+                    </label>
+
+                    <label>
+                        <input type="radio" wire:model="has_specifications" value="0">
+                        No
+                    </label>
+                </div>
+
+                @error('has_specifications')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
             </fieldset>
 
         </div>
 
         <div class="card-footer">
-            <button type="submit" class="btn btn-primary">
+            <button class="btn btn-primary">
                 <i class="fa fa-save"></i> Save Variant
             </button>
 
