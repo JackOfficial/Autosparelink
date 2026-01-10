e10efd45ff2be326e24f8b3cc1737d9f5ae86421
JackOfficial <musengimanajacques@gmail.com>
2026-01-10 08:54:17 +0200

    Prepare for Livewire reinstall and push

--- resources/views/livewire/admin/model-form.blade.php
+++ resources/views/livewire/admin/model-form.blade.php
@@ -1,21 +1,4 @@
 <div>
-<section class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6"><h1>Add Model</h1></div>
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="/admin">Home</a></li>
-                    <li class="breadcrumb-item active">Add Model</li>
-                </ol>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="content">
-<div class="row">
-<div class="col-md-10">
 <div class="card card-primary">
 <div class="card-header">
     <h3 class="card-title">Model Information</h3>
@@ -161,7 +144,4 @@
 </form>
 </div>
 </div>
-</div>
-</div>
-</section>
 </div>
