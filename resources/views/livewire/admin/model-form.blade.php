72155efabffd56576e77c6a1ba4022bcbb476bef
JackOfficial <musengimanajacques@gmail.com>
2026-01-10 08:44:22 +0200

    Prepare for Livewire reinstall and push

--- resources/views/livewire/admin/model-form.blade.php
+++ resources/views/livewire/admin/model-form.blade.php
@@ -1,7 +1,15 @@
 <div>
 <section class="content-header">
     <div class="container-fluid">
-        <h1>Add Vehicle Model ({{ $has_variants }})</h1>
+        <div class="row mb-2">
+            <div class="col-sm-6"><h1>Add Model</h1></div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="/admin">Home</a></li>
+                    <li class="breadcrumb-item active">Add Model</li>
+                </ol>
+            </div>
+        </div>
     </div>
 </section>
 
