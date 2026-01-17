9d43e4aac3d8559cca310d076875312e0fcceb80
JackOfficial <musengimanajacques@gmail.com>
2026-01-17 08:30:54 +0200

    Parts

--- app/Http/Controllers/PartCatalogController.php
+++ app/Http/Controllers/PartCatalogController.php
@@ -20,6 +20,8 @@ class PartCatalogController extends Controller
          |-------------------------------------------------- */
 
         // CASE 1: Model-based specification
+
+        dd("type is: ". $type . " and specification is: " . $specification);
         if (
             $type === 'model' &&
             $specification->model_id &&
