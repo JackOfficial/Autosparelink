fef978e674d1f26174e23838f6d8e83090cf1a68
JackOfficial <musengimanajacques@gmail.com>
2026-01-19 06:22:23 +0200

    Cart System

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\RoleMiddlewareServiceProvider::class,
+    // Add the Darryldecode provider here
+    Darryldecode\Cart\CartServiceProvider::class,
 ];
