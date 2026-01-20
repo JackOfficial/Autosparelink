7ea89640f42c3605570c81a5cbca6ae9868bd022
JackOfficial <musengimanajacques@gmail.com>
2026-01-20 17:31:00 +0200

    Cart System

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,9 +40,9 @@
             </div>
           </div>
           @error('name')
-        <span class="text-primary" role="alert">
+        <div class="text-primary" role="alert">
             <strong>{{ $message }}</strong>
-        </span>
+        </div>
         @enderror
       </div>
 
@@ -54,9 +54,9 @@
             </div>
           </div>
           @error('email')
-        <span class="text-primary" role="alert">
+        <div class="text-primary" role="alert">
             <strong>{{ $message }}</strong>
-        </span>
+        </div>
         @enderror
       </div>
 
@@ -68,9 +68,9 @@
             </div>
           </div>
           @error('password')
-            <span class="invalid-feedback" role="alert">
+            <div class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
-            </span>
+            </div>
           @enderror
         </div>
 
