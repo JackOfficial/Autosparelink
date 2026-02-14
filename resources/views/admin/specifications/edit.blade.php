e2661a7719cda628b0f3c70d2b6869de5ba2c27c
JackOfficial <musengimanajacques@gmail.com>
2026-02-14 10:15:24 +0200

    Blogging System

--- resources/views/admin/specifications/edit.blade.php
+++ resources/views/admin/specifications/edit.blade.php
@@ -19,9 +19,7 @@
 <section class="content">
 <div class="row">
 <div class="col-md-11">
-     <livewire:admin.specifications.edit-specification
-    :specificationId="request()->query('specification_id')"
-/>
+  
 </div>
 </div>
 </section>
