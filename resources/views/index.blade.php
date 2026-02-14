0e5ef1604494a9107b9fa84392df63d0f95ebfa9
JackOfficial <musengimanajacques@gmail.com>
2026-02-14 19:24:07 +0200

    Blogging System

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -275,7 +275,7 @@
 <div class="container-fluid pt-4 pb-3">
     <div class="section-header px-xl-5">
         <h2>Featured Spare Parts</h2>
-        <a href="/shop/products" class="text-decoration-none">See all featured →</a>
+        <a href="/spare-parts" class="text-decoration-none">See all featured →</a>
     </div>
     <div class="row px-xl-5">
         @forelse ($parts as $part)
@@ -290,7 +290,7 @@
 <div class="container-fluid pt-4 pb-3">
     <div class="section-header px-xl-5">
         <h2>Recent Spare Parts</h2>
-        <a href="/shop/products" class="text-decoration-none">See all recent →</a>
+        <a href="/spare-parts" class="text-decoration-none">See all recent →</a>
     </div>
     <div class="row px-xl-5">
         @forelse ($recent_parts as $recent_part)
