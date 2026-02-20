ebfe94f87cb64b6b62c26ecae15d8f86817e3a40
JackOfficial <musengimanajacques@gmail.com>
2026-02-20 08:49:02 +0200

    Payment Orders

--- app/Http/Controllers/VinController.php
+++ app/Http/Controllers/VinController.php
@@ -37,6 +37,7 @@ public function search(Request $request)
 
     // 2. Perform your ID matching here to help Livewire
     $brand = Brand::whereRaw('UPPER(brand_name) = ?', [strtoupper($vehicle['General Information']['Make'])])->first();
+    dd($brand);
     $model = $brand ? VehicleModel::where('brand_id', $brand->id)->where('model_name', 'LIKE', '%'.$vehicle['General Information']['Model'].'%')->first() : null;
 
     // 3. Pass everything to the blade
