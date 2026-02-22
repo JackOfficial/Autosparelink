bbaad68dd17d8caac7c7fd46c9e81ec748f82fc0
JackOfficial <musengimanajacques@gmail.com>
2026-02-23 01:48:49 +0200

    Payment Orders

--- resources/views/livewire/specifications/index.blade.php
+++ resources/views/livewire/specifications/index.blade.php
@@ -66,23 +66,11 @@
                     </div>
                     
                     <div class="col-lg-4 text-center text-lg-right mt-4 mt-lg-0">
-                       <div class="col-lg-4 text-center text-lg-right mt-4 mt-lg-0 position-relative">
-    @if($item->vehicleModel->brand->brand_logo)
-        <div class="d-inline-block bg-white p-3 rounded shadow-lg mb-2 logo-container" style="border-radius: 15px !important;">
-            <img src="{{ asset('storage/' . $item->vehicleModel->brand->brand_logo) }}" style="height: 80px; width: 130px; object-fit: contain;" alt="Brand Logo" />
-        </div>
-    @endif
-
-    {{-- Targeted & Absolutely Positioned Loading --}}
-    <div wire:loading wire:target="search, destination, drive, steering_position, resetFilters" 
-         class="position-absolute w-100" 
-         style="bottom: -25px; right: 0;">
-        <div class="d-flex align-items-center justify-content-center justify-content-lg-end">
-            <div class="spinner-border spinner-border-sm text-primary mr-2" role="status"></div>
-            <span class="text-white-50 small font-weight-bold">Refreshing results...</span>
-        </div>
-    </div>
-</div>
+                        @if($item->vehicleModel->brand->brand_logo)
+                            <div class="d-inline-block bg-white p-3 rounded shadow-lg mb-2 logo-container" style="border-radius: 15px !important;">
+                                <img src="{{ asset('storage/' . $item->vehicleModel->brand->brand_logo) }}" style="height: 80px; width: 130px; object-fit: contain;" alt="Brand Logo" />
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
