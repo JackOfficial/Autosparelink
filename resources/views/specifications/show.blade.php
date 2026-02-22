22c231e7a7b2a5155fd0e77d8baaa3b0fe8e2efd
JackOfficial <musengimanajacques@gmail.com>
2026-02-22 04:23:26 +0200

    Payment Orders

--- resources/views/specifications/show.blade.php
+++ resources/views/specifications/show.blade.php
@@ -14,7 +14,7 @@
 @section('content')
 
 <div class="container-fluid">
-    <div class="row px-xl-5">
+    <div class="row px-xl-5 mt-2">
         <div class="col-12">
             <nav class="breadcrumb bg-light mb-30">
                 <a class="breadcrumb-item text-dark" href="{{ route('home') }}">Home</a>
