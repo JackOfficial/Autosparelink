39644693ddc999c2d84ae8af6591c6942cb87cc6
JackOfficial <musengimanajacques@gmail.com>
2026-03-08 17:28:54 +0200

    VIN

--- app/Http/Controllers/VinController.php
+++ app/Http/Controllers/VinController.php
@@ -152,7 +152,7 @@ $parts = Part::whereHas('fitments', function($q) use ($variantSpecs) {
 ->with(['photos', 'category', 'partBrand']) // optional eager load
 ->get();
 
-    dd($variantSpecs); // Use this to verify the final match!
+    dd($parts); // Use this to verify the final match!
 
     return view('parts.index', [
         'brandId' => $brand->id,
