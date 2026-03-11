9b7ec9c9b16760411d3aee701376a2f4d36723f5
JackOfficial <musengimanajacques@gmail.com>
2026-03-11 17:13:26 +0200

    VIN Search Integration

--- app/Livewire/Parts/PartsCatalog.php
+++ app/Livewire/Parts/PartsCatalog.php
@@ -61,7 +61,7 @@ class PartsCatalog extends Component
             $this->vinData = $vinData;
             $this->brand = $vinData['brand_id'] ?? null;
             $this->model = $vinData['model_id'] ?? null;
-            dd($variant);
+            dd($brand);
 
             // Note: If you have a specific variant from VIN, set it here
         } else {
