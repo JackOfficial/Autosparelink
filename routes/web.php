c1d66668eaa690c70f55c230021746fbfe44bd27
JackOfficial <musengimanajacques@gmail.com>
2026-03-15 09:52:35 +0200

    VIN Decoder

--- routes/web.php
+++ routes/web.php
@@ -165,7 +165,7 @@ Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.in
 Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.store');
 Route::resource('orders', OrderController::class)->middleware('auth');
 
-Route::get('/payment/process/{order}', [PaymentController::class, 'process'])->name('payment.process');
+Route::get('/payment/process/{order}', [FlutterwavePaymentController::class, 'process'])->name('payment.process');
 
 Route::middleware(['auth'])->group(function () {
     
