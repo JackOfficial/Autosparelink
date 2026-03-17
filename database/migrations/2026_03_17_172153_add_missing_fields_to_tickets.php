2e6ad6d6b20d13cf1635896504e563f821260310
JackOfficial <musengimanajacques@gmail.com>
2026-03-17 19:22:35 +0200

    VIN Decoder

---
+++ database/migrations/2026_03_17_172153_add_missing_fields_to_tickets.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            if (!Schema::hasColumn('tickets', 'category')) {
+            $table->string('category')->after('user_id');
+        }
+        if (!Schema::hasColumn('tickets', 'order_ref')) {
+            $table->string('order_ref')->nullable()->after('category');
+        }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            //
+        });
+    }
+};
