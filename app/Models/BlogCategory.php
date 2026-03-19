6091560185f174421d334f2339ac07e95d18c946
JackOfficial <musengimanajacques@gmail.com>
2026-03-19 21:19:24 +0200

    VIN Decoder

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BlogCategory extends Model
 {
@@ -18,7 +19,19 @@ class BlogCategory extends Model
         'type'
     ];
     
-    public function blogs(){
-        return $this->hasMany(Blog::class);
+  /**
+     * Relationship to the Blogs
+     */
+    public function blogs(): HasMany
+    {
+        return $this->hasMany(Blog::class, 'blog_category_id');
+    }
+
+    /**
+     * Relationship to the News
+     */
+    public function news(): HasMany
+    {
+        return $this->hasMany(News::class, 'blog_category_id');
     }
 }
