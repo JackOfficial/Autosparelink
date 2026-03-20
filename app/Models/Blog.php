8f37a21ec87cbed9effac61762ed8808c2e5e4f5
JackOfficial <musengimanajacques@gmail.com>
2026-03-20 09:12:24 +0200

    VIN Decoder

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Blog extends Model
 {
@@ -16,31 +20,49 @@ class Blog extends Model
         'title',
         'slug',
         'content',
+        'status', // Added this to prevent the "Unknown column 'status'" error
     ];
     
-    public function user(){
+    /**
+     * Get the user that authored the blog post.
+     */
+    public function user(): BelongsTo
+    {
         return $this->belongsTo(User::class);
     }
 
-    // Inside App\Models\Blog.php
-
-public function category()
-{
-    // Explicitly mapping blog_category_id to the category model
-    return $this->belongsTo(BlogCategory::class, 'blog_category_id');
-}
+    /**
+     * Get the category the blog belongs to.
+     */
+    public function category(): BelongsTo
+    {
+        // Explicitly mapping blog_category_id to the category model
+        return $this->belongsTo(BlogCategory::class, 'blog_category_id');
+    }
     
-    public function blogPhoto()
-{
-    return $this->morphOne(Photo::class, 'imageable');
-}
+    /**
+     * Get the blog's featured photo (Polymorphic).
+     */
+    public function blogPhoto(): MorphOne
+    {
+        return $this->morphOne(Photo::class, 'imageable');
+    }
     
-    public function likes()
-{
-    return $this->hasMany(Like::class);
-}
+    /**
+     * Get the likes for the blog post.
+     */
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
 
-public function comments(){
-    return $this->hasMany(Comment::class);
-}
+    /**
+     * Get all of the blog's comments (Polymorphic).
+     * This fixes the "Unknown column 'blog_id'" error by looking for 
+     * 'commentable_id' and 'commentable_type' instead.
+     */
+    public function comments(): MorphMany
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
 }
