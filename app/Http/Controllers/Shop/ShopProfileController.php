936233bebf32f9482945f28a5bbd008f4053798f
JackOfficial <musengimanajacques@gmail.com>
2026-04-10 18:11:27 +0200

    fixing

--- app/Http/Controllers/Shop/ShopProfileController.php
+++ app/Http/Controllers/Shop/ShopProfileController.php
@@ -53,7 +53,7 @@ class ShopProfileController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update()
     {
        $shop = Auth::user()->shop;
 
