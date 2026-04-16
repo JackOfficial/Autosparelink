bbb68fa28b1b3c01833532417d2f72a919400f7d
JackOfficial <musengimanajacques@gmail.com>
2026-04-16 05:29:12 +0200

    fixing

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,6 +5,7 @@
     <title>@yield('title') | AutoSpare Link Dashboard</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet">
     
     <style>
