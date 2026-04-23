a686785d362efcca90e2b39b56e1bf6ca70b1f43
JackOfficial <musengimanajacques@gmail.com>
2026-04-23 04:10:02 +0200

    fixing

--- app/Http/Controllers/PartCatalogController.php
+++ app/Http/Controllers/PartCatalogController.php
@@ -26,7 +26,6 @@ class PartCatalogController extends Controller
 
 public function part_for_specification($brand, $model, $slug)
 {
-    dd('here');
     // 1. Fetch the spec by slug (our unique identifier)
     $specification = Specification::where('slug', $slug)
         ->with(['vehicleModel.brand'])
