d2bb0d10e6e1b2ace35732ce4c2dfc1718cd5e18
JackOfficial <musengimanajacques@gmail.com>
2026-04-24 05:43:02 +0200

    fixing

--- database/migrations/2025_12_05_071146_create_payments_table.php
+++ database/migrations/2025_12_05_071146_create_payments_table.php
@@ -19,9 +19,7 @@ return new class extends Migration
           ->constrained('orders')
           ->cascadeOnDelete();
 
-           $table->foreignId('user_id')
-          ->constrained('users')
-          ->cascadeOnDelete();
+          $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnDelete();
 
 
     // Payment details
