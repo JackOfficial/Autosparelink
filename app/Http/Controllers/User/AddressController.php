c49b16f36953c3e9304a05abbb518d437406c83f
JackOfficial <musengimanajacques@gmail.com>
2026-05-01 13:37:45 +0200

    InTouch API Integration

--- app/Http/Controllers/User/AddressController.php
+++ app/Http/Controllers/User/AddressController.php
@@ -92,7 +92,7 @@ class AddressController extends Controller
 
         $address->update($validated);
 
-        return redirect()->route('addresses.index')
+        return redirect()->route('users.addresses.index')
             ->with('success', 'Address updated successfully.');
     }
 
