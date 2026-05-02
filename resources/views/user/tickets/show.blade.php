4c953baf736d31d0d2ad0bbff79a9ce3a053019d
JackOfficial <musengimanajacques@gmail.com>
2026-05-02 18:41:10 +0200

    InTouch API Integration

--- resources/views/user/tickets/show.blade.php
+++ resources/views/user/tickets/show.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.dashboard')
-
+@section('title', 'Tickets')
 @section('content')
     {{-- Pass the ID variable to the component --}}
     <livewire:tickets.show :id="$ticket->id" />
