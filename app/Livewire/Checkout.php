ff59692e4a6d08a090004aaf383baf9e1a9cbbeb
JackOfficial <musengimanajacques@gmail.com>
2026-07-10 20:26:19 +0200

    Editing and quick fixes

--- app/Livewire/Checkout.php
+++ app/Livewire/Checkout.php
@@ -255,10 +255,10 @@ class Checkout extends Component
             }
 
             $localTransactionId = 'AST-' . strtoupper(Str::random(10));
-
+dd("Hello");
             // Extract order and items generation to centralized method
             $order = $this->createOrder($final_address_id, $totalOrderAmount, $shippingFee, $orderStatus, $localTransactionId, $city);
-            dd($paymentPhone, $payableNow, $localTransactionId);
+            
             $response = $inTouch->requestPayment($paymentPhone, $payableNow, $localTransactionId);
             
             if ($response && isset($response['success']) && $response['success'] == true) {
