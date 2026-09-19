be7b2113c5c4a0e5b1df14cbfd34db88f7a32871
JackOfficial <musengimanajacques@gmail.com>
2026-09-19 07:00:29 +0200

    Editing and quick fixes

--- resources/views/livewire/checkout.blade.php
+++ resources/views/livewire/checkout.blade.php
@@ -266,7 +266,7 @@
                                         @if($payment_method === 'cod')
                                             Pay Shipping Fee: {{ number_format($shippingFee, 0) }} RWF
                                         @else
-                                            Pay {{ number_format($subtotal + $shippingFee, 0) }} RWF
+                                            Pay {{ $subtotal + $shippingFee }} RWF
                                         @endif
                                     </span>
                                     <span wire:loading wire:target="placeOrder">
